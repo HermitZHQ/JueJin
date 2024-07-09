336c1ee9470513d5ad108ee2c6044b3d02606113
Bwv1068ii <rain8865@hotmail.com>
2024-07-09 20:04:01 +0800

    更改存入昨日历史数据方式，添加昨日历史数据的25分钟集合进价数据

--- 7e863bef-3d91-11ef-bcc2-00ff2b50aff6/main.py
+++ 7e863bef-3d91-11ef-bcc2-00ff2b50aff6/main.py
@@ -32,7 +32,11 @@ class DownloadHistoryC():
         self.ids_path_a1 = 'c:\\TradeLogs\\IDs-' + self.str_load_ids_strategy + '-A1.txt'
 
         self.subscription_stock_arr = []
-        self.his_data = {}
+        self.his_25_amount_data_arr = []
+        self.his_data_arr = []
+        self.notin_25_stock_arr = []
+
+        self.his_data_dic = {}
         
         self.load_ids(context)
 
@@ -65,6 +69,9 @@ class DownloadHistoryC():
         #测试的时候用，重新赋值，不用后面老是替换了,不用的时候注释掉
         # s_time = test_s_time
         # e_time = test_e_time
+        #历史数据中，集合进价时间
+        s_25_hisroty_time = str(yesterday_date) + ' 09:25:00'
+        e_25_hisroty_time = str(yesterday_date) + ' 09:25:00'
 
         print(f"{s_time} To {e_time}")
         print(f"SECTION_HISTORY_STOCK_COUNT::{self.SECTION_HISTORY_STOCK_COUNT}")
@@ -76,6 +83,7 @@ class DownloadHistoryC():
 
         #这里我们用一个临时集合来接取数据，再将临时集合中的数据添加到his_data中
         temp_his_data = []
+        temp_his_25_amount_data = []
         #先将存入集合中的标的代码，以SECTION_HISTORY_STOCK_COUNT为准拼接未字符串，用来分批次获得历史数据
         for item in self.context.subscription_stock_arr:
                 if temp_index == 0:
@@ -89,17 +97,27 @@ class DownloadHistoryC():
                     temp_his_data = history(symbol=temp_ids_str, frequency='60s', start_time=s_time,  end_time=e_time, fields='symbol, amount, eob, name', adjust=ADJUST_PREV, df=False)
                     print(f"temp_his_data length::{len(temp_his_data)}")
 
+                    temp_his_25_amount_data = history(symbol=temp_ids_str, frequency='tick', start_time=s_25_hisroty_time,  end_time=e_25_hisroty_time, fields='symbol, last_amount, created_at', skip_suspended=False, fill_missing='NaN', adjust=ADJUST_PREV, df=False)
+
                     #重置temp_index以及temp_ids_str
                     temp_index = 0
                     temp_ids_str = ''
+
+                    for temp_data in temp_his_25_amount_data:
+                        #这里应该要新创建一个变量
+                        temp_temp_data = temp_data
+                        # self.his_data[temp_history_dic_index] = {'symbol':temp_data['symbol'], 'amount':temp_data['last_amount'], 'eob':temp_data['created_at']}
+                        self.his_25_amount_data_arr.append(temp_temp_data)
+                        # temp_history_dic_index += 1
 
                     #将temp_his_data装入his_data里面
                     for temp_data in temp_his_data:
                         #这里应该要新创建一个变量
                         temp_temp_data = temp_data
                         # self.his_data.append(temp_temp_data)
-                        self.his_data[temp_history_dic_index] = temp_temp_data
-                        temp_history_dic_index += 1
+                        # self.his_data[temp_history_dic_index] = temp_temp_data
+                        self.his_data_arr.append(temp_temp_data)
+                        # temp_history_dic_index += 1
 
                     #重置temp_his_data方便接受下一批次
                     temp_his_data.clear()
@@ -110,21 +128,60 @@ class DownloadHistoryC():
             temp_his_data = history(symbol=temp_ids_str, frequency='60s', start_time=s_time,  end_time=e_time, fields='symbol, amount, eob, name', adjust=ADJUST_PREV, df=False)
             print(f"temp_his_data length::{len(temp_his_data)}")
 
-            print(f"temp_ids_str:{temp_ids_str}")
+            temp_his_25_amount_data = history(symbol=temp_ids_str, frequency='tick', start_time=s_25_hisroty_time,  end_time=e_25_hisroty_time, fields='symbol, last_amount, created_at', skip_suspended=False, fill_missing='NaN', adjust=ADJUST_PREV, df=False)
+
+            print(f"temp_his_data length::{len(temp_his_25_amount_data)}")
 
             temp_ids_str = ''
+
+            for temp_data in temp_his_25_amount_data:
+                #这里应该要新创建一个变量
+                temp_temp_data = temp_data
+                # self.his_data[temp_history_dic_index] = {'symbol':temp_data['symbol'], 'amount':temp_data['last_amount'], 'eob':temp_data['created_at']}
+                self.his_25_amount_data_arr.append(temp_temp_data)
+                # temp_history_dic_index += 1
 
             #将temp_his_data装入his_data里面
             for temp_data in temp_his_data:
                 #这里应该要新创建一个变量
                 temp_temp_data = temp_data
-                # self.his_data.append(temp_temp_data)
-                self.his_data[temp_history_dic_index] = temp_temp_data
-                temp_history_dic_index += 1
+                # self.his_data[temp_history_dic_index] = temp_temp_data
+                self.his_data_arr.append(temp_temp_data)
+                # temp_history_dic_index += 1
 
             temp_his_data.clear()
 
-        print(f"his_data length::{len(self.his_data)}")
+        temp_25_stock_arr = []
+        for item in self.his_25_amount_data_arr:
+            temp_25_stock_arr.append(item['symbol'])
+
+        for temp_stock in self.context.subscription_stock_arr:
+            if temp_stock not in temp_25_stock_arr:
+                self.notin_25_stock_arr.append(temp_stock)
+
+        #将所有数据添加到his_data中
+        temp_history_dic_index = 0
+        for i in range(2):
+            if i == 0:
+                test_yesterday_eob = str(yesterday_date) + " " + "09:25:00+08:00"
+            else:
+                test_yesterday_eob = str(yesterday_date) + " " + "09:26:00+08:00"
+
+            #先将25min数据装入dic
+            for his_25_val in self.his_25_amount_data_arr:
+                self.his_data_dic[temp_history_dic_index] = {'symbol':his_25_val['symbol'], 'amount':his_25_val['last_amount'], 'eob':test_yesterday_eob}
+                temp_history_dic_index += 1
+            #将没有数据的标的，赋予0.0值装入dic
+            for notin_his_25_val in self.notin_25_stock_arr:
+                self.his_data_dic[temp_history_dic_index] = {'symbol':notin_his_25_val, 'amount':0.0, 'eob':test_yesterday_eob}
+                temp_history_dic_index += 1
+
+        for item in self.his_data_arr:
+            temp_item = item
+            self.his_data_dic[temp_history_dic_index] = temp_item
+            temp_history_dic_index += 1
+
+        print(f"his_data length::{len(self.his_data_dic)}")
 
     def load_ids(self, context):
         # 看股票代码就能分辨，上证股票是在上海交易所上市的股票，股票代码以600、601、603开头，科创板（在上海交易所上市）股票代码以688开头
@@ -199,13 +256,16 @@ class DownloadHistoryC():
 
 
     def save_history_info(self):
-        np.save(self.save_history_path, self.his_data)
+        np.save(self.save_history_path, self.his_data_dic)
 
     def load_history_info(self):
         temp_history_dic = np.load(self.load_history_path, allow_pickle=True)
         temp_history_dic = dict(temp_history_dic.tolist())
 
-        print(f"temp_history_dic length:{len(temp_history_dic)}")
+        # for value in temp_history_dic.values():
+        #     print(f"{value['symbol']}:{value['amount']}:{value['eob']}")
+
+        print(f"load history success :{len(temp_history_dic)}")
 
 
 # 策略中必须有init方法
@@ -216,7 +276,7 @@ def init(context):
     get_history = DownloadHistoryC(context)
     get_history.download_history()
     get_history.save_history_info()
-    # get_history.load_history_info()
+    get_history.load_history_info()
 
     pass
 
